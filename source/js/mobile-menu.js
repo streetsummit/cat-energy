66eed0f9b78028b1775fe3bd69a52b9469faf826
streetsummit <streetsummit@mail.ru>
2021-01-06 00:29:54 +0300

    Правки по ПП

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -11,4 +11,10 @@ navToggle.addEventListener('click', function () {
   navMain.classList.toggle('main-nav--opened');
   navToggle.classList.toggle('burger-button--closed');
   navToggle.classList.toggle('burger-button--opened');
+
+  if (navToggle.classList.contains('burger-button--opened')) {
+    navToggle.setAttribute('aria-label', 'Закрыть меню');
+  } else {
+    navToggle.setAttribute('aria-label', 'Открыть меню');
+  }
 });
